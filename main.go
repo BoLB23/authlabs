cb4c8c1eae64aeb1f84d5177cc60e491377ae2a4
Bill Schmidt <schmidtwilliam01@gmail.com>
2022-01-05 21:48:09 -0500

    another one

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 	"os"
 
-	auth "github.com/BoLB23/authlabs/auth"
-	ControllerEvents "github.com/BoLB23/authlabs/controllers/events"
+	"github.com/BoLB23/authlabs/auth"
+	"github.com/BoLB23/authlabs/controllers/events"
 	"github.com/BoLB23/authlabs/controllers/handlers"
 	ControllerHome "github.com/BoLB23/authlabs/controllers/home"
 	Logging "github.com/BoLB23/authlabs/controllers/logging"
@@ -39,11 +39,11 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", ControllerHome.HomeLink)
 	router.HandleFunc("/login", auth.Login).Methods("POST")
-	router.HandleFunc("/event", ControllerEvents.CreateEvent).Methods("POST")
-	router.HandleFunc("/events", ControllerEvents.GetAllEvents).Methods("GET")
-	router.HandleFunc("/events/{id}", ControllerEvents.GetOneEvent).Methods("GET")
-	router.HandleFunc("/events/{id}", ControllerEvents.UpdateEvent).Methods("PATCH")
-	router.HandleFunc("/events/{id}", ControllerEvents.DeleteEvent).Methods("DELETE")
+	router.HandleFunc("/event", events.CreateEvent).Methods("POST")
+	router.HandleFunc("/events", events.GetAllEvents).Methods("GET")
+	router.HandleFunc("/events/{id}", events.GetOneEvent).Methods("GET")
+	router.HandleFunc("/events/{id}", events.UpdateEvent).Methods("PATCH")
+	router.HandleFunc("/events/{id}", events.DeleteEvent).Methods("DELETE")
 	router.Use(Logging.RequestLoggerMiddleware(router))
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
